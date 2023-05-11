412cc52c4df2b95ee6f91c8e0e88efe37167aad7
marobert <marobert@student.42lyon.fr>
2023-05-11 15:47:37 +0200

    added mlx_error print when mlx init function fail

--- sources/utils/window_utils.c
+++ sources/utils/window_utils.c
@@ -6,7 +6,7 @@
 /*   By: marobert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 15:08:18 by marobert          #+#    #+#             */
-/*   Updated: 2023/05/11 15:33:33 by marobert         ###   ########.fr       */
+/*   Updated: 2023/05/11 15:42:13 by marobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,8 @@ void	put_pxl_img(t_img *img, int x, int y, int color)
 void	init_img(t_window *window)
 {
 	window->img.img = mlx_new_image(window->mlx_ptr, W_WIDTH, W_HEIGHT);
+	if (!window->img.img)
+		exit(printf("mlx error\n"));
 	window->img.addr = mlx_get_data_addr(window->img.img, \
 		&window->img.bpp, &window->img.line_length, \
 		&window->img.endian);
@@ -64,7 +66,11 @@ t_game	*init_game(t_map *map)
 	if (!game || !window)
 		exit(1);
 	window->mlx_ptr = mlx_init();
+	if (!window->mlx_ptr)
+		exit(printf("mlx error\n"));
 	window->win_ptr = mlx_new_window(window->mlx_ptr, W_WIDTH, W_HEIGHT, TITLE);
+	if (!window->win_ptr)
+		exit(printf("mlx error\n"));
 	game->player = init_player(map);
 	game->map = map;
 	game->map->floor = 0x797c7d;
